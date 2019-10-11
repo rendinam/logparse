67402cb444ad02e61ae1d5e7680c4674250ea307
Matt Rendina <mrendina@stsci.edu>
2019-10-11 13:49:00 -0400

    Use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='conmets',
-    #use_scm_version=True,
-    #setup_requires=['setuptools_scm'],
-    version='0.0.1',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    #version='0.0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [

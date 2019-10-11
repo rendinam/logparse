6e7e41924bc1a7735bc1cc003aa4c6f9861fb9c8
Matt Rendina <mrendina@stsci.edu>
2019-10-11 12:05:54 -0400

    Add setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='conmets',
+    #use_scm_version=True,
+    #setup_requires=['setuptools_scm'],
+    version='0.0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'conmets = conmets.main:main',
+        ],
+    },
+    install_requires=[
+        'certifi',
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'PyYAML',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    }
+)
